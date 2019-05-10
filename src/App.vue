702de042fddca3a38c43f5646c0ef54631f90c0a
imaxct <imaxct@hotmail.com>
2019-05-10 21:45:53 +0800

    打包配置修改

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,58 @@
 <template>
-    <div id="app">
-        <loading v-model="isLoading"></loading>
-        <router-view></router-view>
-        <tabbar style="position: fixed">
-            <tabbar-item selected link="/">
-                <img slot="icon" src="./assets/home.png">
-                <span slot="label">首页</span>
-            </tabbar-item>
-            <tabbar-item link="/my">
-                <img slot="icon" src="./assets/user.png">
-                <span slot="label">我的</span>
-            </tabbar-item>
-        </tabbar>
-    </div>
+  <div id="app">
+    <loading v-model="isLoading"></loading>
+    <router-view></router-view>
+    <tabbar style="position: fixed" v-if="$route.name !== 'login'">
+      <tabbar-item selected link="/">
+        <img slot="icon" src="./assets/home.png">
+        <span slot="label">首页</span>
+      </tabbar-item>
+      <tabbar-item link="/my">
+        <img slot="icon" src="./assets/user.png">
+        <span slot="label">我的</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
 </template>
 
 <script>
-import { Loading, Tabbar, TabbarItem } from 'vux'
-import { mapState, mapActions } from 'vuex'
-import { getToken } from '@/libs/util'
+import { Loading, Tabbar, TabbarItem } from "vux";
+import { mapState, mapActions } from "vuex";
+import { getToken } from "@/libs/util";
 
 export default {
-    name: 'app',
-    components: {
-        Loading,
-        mapState,
-        Tabbar,
-        TabbarItem
-    },
-    computed: {
-        ...mapState({
-            isLoading: state => state.loading.isLoading
-        })
-    },
-    methods: {
-        ...mapActions([
-            'handleToken'
-        ]),
-    },
-    beforeMount() {
-        const token = getToken()
-        if (token && token['token']) {
-            this.handleToken(token).then(res => {
-                this.$router.push({
-                    name: 'home'
-                })
-            })
-        }
+  name: "app",
+  components: {
+    Loading,
+    mapState,
+    Tabbar,
+    TabbarItem
+  },
+  computed: {
+    ...mapState({
+      isLoading: state => state.loading.isLoading
+    })
+  },
+  methods: {
+    ...mapActions(["handleToken"])
+  },
+  beforeMount() {
+    const token = getToken();
+    if (token && token["token"]) {
+      this.handleToken(token).then(res => {
+        this.$router.push({
+          name: "home"
+        });
+      });
     }
-}
+  }
+};
 </script>
 
 <style lang="less">
-@import '~vux/src/styles/reset.less';
-@import '~vux/src/styles/1px.less';
+@import "~vux/src/styles/reset.less";
+@import "~vux/src/styles/1px.less";
 body {
-    background-color: #fbf9fe;
+  background-color: #fbf9fe;
 }
 </style>
